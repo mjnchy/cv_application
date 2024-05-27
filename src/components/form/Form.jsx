c6fddd80a0abdf51c4c0de9acb532d27f3616e26
mijan <mijanchowdhury2001@gmail.com>
2024-05-27 17:49:52 -0400

    The form is mostly complete and most styles have been applied.

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,17 +1,45 @@
+import { useState } from "react";
 import Education from "./Education";
 import Experience from "./Experience";
 import Introduction from "./Introduction";
 import Skills from "./Skills";
 
 function Form() {
+  const [segment, setSegment] = useState(0);
+  const segments = [
+    [ <Introduction />, "Introduction" ],
+    [ <Education />, "Education" ],
+    [ <Skills />, "Skills" ],
+    [ <Experience />, "Work Experience" ] 
+  ];
+
   return (
     <form className="form">
-      <Introduction />
-      <Education />
-      <Skills />
-      <Experience />
-      <button type="button">Go back</button>
-      <button type="button">Next</button>
+      {segment < 1 ? null :
+        <button 
+          className="button form-nav-btn prev" 
+          type="button" 
+          onClick={() => setSegment(segment - 1)}>
+          <i className="fa-solid fa-arrow-left"></i>
+          <span className="btn-text-container">Go Back</span>
+        </button>
+      }
+      {segments[segment][0]} 
+      {segment >= segments.length - 1 ? null :
+        <button
+          className="button form-nav-btn next"
+          type="button" 
+          onClick={() => setSegment(segment + 1)}>
+          {`Next: ${segments[segment + 1][1]}`}
+        </button>
+      }
+      {segment < segments.length - 1 ? null :
+        <button 
+          className="button form-nav-btn save" 
+          type="submit">
+          Save
+        </button>
+      }
     </form>
   );
 };

bfe932d444545ebd978050339f13076ee752fa94
mijan <mijanchowdhury2001@gmail.com>
2024-06-09 23:15:54 -0400

    Adding a function to save the form data to a cv object that will be used to fill the input fields and to create the final cv.

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -4,14 +4,38 @@ import Experience from "./Experience";
 import Introduction from "./Introduction";
 import Skills from "./Skills";
 
+const cv = {
+  name: { first: "", last: "" },
+  address: { city: "", region: "", zip: "" },
+  contact: { email: "", phone: ""},
+  schools: [], skills: [], experience: []
+};
+
 function Form() {
   const [segment, setSegment] = useState(0);
   const segments = [
-    [ <Introduction />, "Introduction" ],
+    [ <Introduction />, "Introduction", () => updateCV(false) ],
     [ <Education />, "Education" ],
     [ <Skills />, "Skills" ],
     [ <Experience />, "Work Experience" ] 
   ];
+
+  function updateCV(iterable, type, str) {
+    const formData = [...new FormData(document.querySelector(".form")).entries()];
+    const keys = Object.keys(cv).slice(0, 3);
+
+    if (iterable === false) {
+      let pointer = 0;
+      for (let i = 0; i < keys.length; i++) {
+        const innerKeys = Object.keys(cv[keys[i]]);
+        for (let j = 0; j < innerKeys.length; j++) {
+          cv[keys[i]][innerKeys[j]] = formData[pointer][1];
+          pointer++;
+        };
+      };
+    } else {};
+    setSegment(segment + 1);
+  };
 
   return (
     <form className="form">
@@ -29,7 +53,7 @@ function Form() {
         <button
           className="button form-nav-btn next"
           type="button" 
-          onClick={() => setSegment(segment + 1)}>
+          onClick={() => segments[segment][2]()}>
           {`Next: ${segments[segment + 1][1]}`}
         </button>
       }
